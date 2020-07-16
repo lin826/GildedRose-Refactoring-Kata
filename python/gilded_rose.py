025d03e66627a43661477c43688b6dc03967e2f0
lin826 <liniju826@gmail.com>
2020-07-16 17:51:53 +0800

    Extract item update functions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-qualifiedItems = [
-    "Aged Brie",
-    "Backstage passes to a TAFKAL80ETC concert",
-    "Sulfuras, Hand of Ragnaros"
-]
-
 class GildedRose(object):
 
     def __init__(self, items):
@@ -13,27 +7,36 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name == qualifiedItems[0]:
-                item.decrease_one_sell_in()
-                if item.quality < 50:
-                    item.increase_one_quality()
-                    if item.sell_in < 0:
-                        item.increase_one_quality()
-            elif item.name == qualifiedItems[1]:
-                item.decrease_one_sell_in()
-                if item.sell_in < 0:
-                    item.clear_quality()
-                elif item.sell_in < 10:
-                    if item.quality < 50:
-                        item.increase_one_quality()
-            elif item.name == qualifiedItems[2]:
+            if item.name == "Aged Brie":
+                self.update_AgedBrie()
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.update_Backstage()
+            elif item.name == "Sulfuras, Hand of Ragnaros":
                 continue
             else:
-                item.decrease_one_sell_in()
-                if item.quality > 0:
-                    item.decrease_one_quality()
-                    if item.sell_in < 0:
-                        item.decrease_one_quality()
+                self.update_others()
+
+    def update_AgedBrie(self, item):
+        item.decrease_one_sell_in()
+        if item.quality < 50:
+            item.increase_one_quality()
+            if item.sell_in < 0:
+                item.increase_one_quality()
+                
+    def update_Backstage(self, item):
+        item.decrease_one_sell_in()
+        if item.sell_in < 0:
+            item.clear_quality()
+        elif item.sell_in < 10:
+            if item.quality < 50:
+                item.increase_one_quality()
+
+    def update_others(self, item):
+        item.decrease_one_sell_in()
+        if item.quality > 0:
+            item.decrease_one_quality()
+            if item.sell_in < 0:
+                item.decrease_one_quality()
 
 class Item:
     def __init__(self, name, sell_in, quality):
