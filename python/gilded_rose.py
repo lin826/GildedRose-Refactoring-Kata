e0752a835ce1bf14db3a21217b4250dee27a058b
lin826 <liniju826@gmail.com>
2020-07-16 16:20:42 +0800

    Extract duplicated string constants

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+qualifiedItems = [
+    "Aged Brie",
+    "Backstage passes to a TAFKAL80ETC concert",
+    "Sulfuras, Hand of Ragnaros"
+]
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,27 +13,29 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != qualifiedItems[0] and item.name != qualifiedItems[1]:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != qualifiedItems[2]:
                         item.decrease_one_quality()
             else:
                 if item.quality < 50:
                     item.increase_one_quality()
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == qualifiedItems[1]:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.increase_one_quality()
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.increase_one_quality()
-            if item.name != "Sulfuras, Hand of Ragnaros":
+
+            if item.name != qualifiedItems[2]:
                 item.decrease_one_sell_in()
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != qualifiedItems[0]:
+                    if item.name != qualifiedItems[1]:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != qualifiedItems[2]:
                                 item.decrease_one_quality()
                     else:
                         item.clear_quality()
